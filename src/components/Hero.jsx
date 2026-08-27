ea4635daedb5531a1dfd7b47c3cb6fa33d531fd6
kashi992 <kashifrajput992@gmail.com>
2026-08-27 12:20:50 +0500

    hero carousal updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,9 +2,9 @@ import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 
 const CAROUSEL_IMAGES = [
+   "/images/Mal_underpass.jpg",
   "/images/PORTAL_HR_SOUTH_v03.jpg",
   "/images/PORTAL_HR_SOUTH2_v02.jpg",
-  "/images/Mal_underpass.jpg",
   "/images/DUSK-SHOT-HR_v02.webp",
   "/images/Hoteo_v02.webp",
   "/images/PORTAL_HR_NORTH_v02.webp",
